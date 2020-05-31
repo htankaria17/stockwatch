d3562191b12651447c58a10db14a3de6e8d0ae0a
htankaria17 <htankaria17@gmail.com>
2020-05-30 20:53:54 -0400

    update comments, data export.

--- stock_data_acq.py
+++ stock_data_acq.py
@@ -5,7 +5,7 @@ import csv
 import datetime
 
 #try:
-FileWrite=1
+FileWrite=0
 
 #stock_name = "XPP.L"
 stock_name = "NESTLEIND.NS"
@@ -18,7 +18,6 @@ stock_sustain = stock_obj.get_sustainability()
 stock_splits=stock_obj.get_splits()
 # get historical market data, here max is 5 years.
 stock_history = stock_obj.history(period="max")
-
 
 try:
     if(FileWrite):
@@ -34,30 +33,9 @@ except:
     print(sys.exc_info())
 
 
-"""
-returns:
-{
- 'quoteType': 'EQUITY',
- 'quoteSourceName': 'Nasdaq Real Time Price',
- 'currency': 'USD',
- 'shortName': 'Microsoft Corporation',
- 'exchangeTimezoneName': 'America/New_York',
-  ...
- 'symbol': 'MSFT'
-}
-"""
+def f_clean():
+    pass
 
-
-
-"""
-returns:
-              Open    High    Low    Close      Volume  Dividends  Splits
-Date
-1986-03-13    0.06    0.07    0.06    0.07  1031788800        0.0     0.0
-1986-03-14    0.07    0.07    0.07    0.07   308160000        0.0     0.0
-...
-2019-11-12  146.28  147.57  146.06  147.07    18641600        0.0     0.0
-2019-11-13  146.74  147.46  146.30  147.31    16295622        0.0     0.0
-"""
+f_clean()
 #except:
 #   print("Stock not found")
